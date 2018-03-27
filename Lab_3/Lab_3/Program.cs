b830106b22021009bea21b0a8e2202c863eb2fa1
Toolmaster <kuba.kowalczykowski@student.put.poznan.pl>
2018-03-27 15:38:51 +0200

    Poprawka
    Poprawione na labach

--- Lab_3/Lab_3/Program.cs
+++ Lab_3/Lab_3/Program.cs
@@ -14,7 +14,17 @@ namespace Lab_3
         {
             while (true)
             {
-                int ilosc_iteracji = 20;
+
+                int ilosc_iteracji = 10;
+                int[] zakres = new int[2];
+                zakres[0] = 1000;
+                zakres[1] = 10500000;
+                int wielkosci_tablic = 75000;
+
+                dataCenter[] ourDatas = new dataCenter[ilosc_iteracji];
+                for (int i = 0; i < ilosc_iteracji; i++)
+                    ourDatas[i] = new dataCenter(wielkosci_tablic, zakres[0], zakres[1]);
+
                 Console.WriteLine("Wybierz metode: ");
                 Console.WriteLine("1. Iteracyjnie");
                 Console.WriteLine("2. Watkowo");
@@ -26,18 +36,18 @@ namespace Lab_3
                 {
                     case 1:
                         {
-                            Iterating(ilosc_iteracji);
+                            Iterating(ilosc_iteracji, ourDatas);
                             break;
                         }
                     case 2:
                         {
-                            Threading(ilosc_iteracji);
+                            Threading(ilosc_iteracji, ourDatas);
                             break;
                         }
                     case 3:
                         {
-                            Iterating(ilosc_iteracji);
-                            Threading(ilosc_iteracji);
+                            Iterating(ilosc_iteracji, ourDatas);
+                            Threading(ilosc_iteracji, ourDatas);
                             break;
                         }
                     default:
@@ -46,19 +56,18 @@ namespace Lab_3
                         }
 
                 }
-                //Console.ReadLine();
+                Console.ReadLine();
             }
         }
 
-        static void Iterating(int ilosc)
+        static void Iterating(int ilosc, dataCenter[] ourDatas)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            dataCenter[] ourDatas = new dataCenter[ilosc];
+
             for (int i = 0; i < ilosc; i++)
             {
-                ourDatas[i] = new dataCenter(1000, 350000);
                 ourDatas[i].arrayGenerator();
                 ourDatas[i].arraySorter();
                 ourDatas[i].onSqr();
@@ -74,21 +83,24 @@ namespace Lab_3
 
         }
 
-        static void Threading(int ilosc)
+        static void Threading(int ilosc, dataCenter[] ourDatas)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
             Threads[] threads = new Threads[ilosc];
+
             for (int i = 0; i < ilosc; i++)
             {
-                threads[i] = new Threads();
+                threads[i] = new Threads(ourDatas[i], i);
             }
 
             sw.Stop();
             float ticks = sw.ElapsedTicks;
             float elapsedTime = ticks / Stopwatch.Frequency;
             Console.WriteLine("Czas wykonywania watkowego: " + elapsedTime + "s");
+
+            
         }
     }
 
@@ -97,47 +109,78 @@ namespace Lab_3
 
 class Threads
 {
-    public Threads()
+    public Threads(dataCenter ourDatas, int i)
     {
-        thread = new Thread(handleObject);
-        thread.Start();
+        ThreadStart pts = delegate
+        {
+            generateObject(ourDatas);
+        };
+        ThreadStart pts2 = delegate
+        {
+            sortObject(ourDatas);
+        };
+        ThreadStart pts3 = delegate
+        {
+            calculateObject(ourDatas);
+        };
+        IDwatku = i;
+        generating = new Thread(pts);
+        sorting = new Thread(pts2);
+        calculations = new Thread(pts3);
+        generating.Start();
     }
     ~Threads()
-    {
+    {}
 
+    public void generateObject(dataCenter ourDatas)
+    {
+        Console.WriteLine("Generuje watek: " + IDwatku + "\n");
+        ourDatas.arrayGenerator();
+        sorting.Start();
     }
 
-    public void handleObject()
+    public void sortObject(dataCenter ourDatas)
+    {
+        Console.WriteLine("Sortuje watek: " + IDwatku + "\n");
+        ourDatas.arraySorter();
+        calculations.Start();
+    }
+
+    public void calculateObject(dataCenter ourDatas)
     {
         //Console.WriteLine("Start watka.");
-        Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)127;
-        ourDatas = new dataCenter(1000, 350000);
-        ourDatas.arrayGenerator();
-        ourDatas.arraySorter();
+        //Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)127;
+        //ourDatas.arrayGenerator();
+        //ourDatas.arraySorter();
+        Console.WriteLine("Kalkuluje watek: " + IDwatku + "\n");
         ourDatas.onSqr();
         ourDatas.inSqr();
         ourDatas.difference();
         ourDatas.calculateSet();
-        //Console.WriteLine("Koniec watka.");
+        Console.WriteLine("Skonczylem watek: " + IDwatku + "\n");
+        //Console.WriteLine("Koniec watku.");
     }
 
-    Thread thread;
-    dataCenter ourDatas;
+    Thread generating;
+    Thread sorting;
+    Thread calculations;
+    int IDwatku;
 }
 
 
 class dataCenter
 {
-    public dataCenter(int Min, int Max)
+    public dataCenter(int wielkosc_tab, int Min, int Max)
     {
-        randoms = new double[Max - Min];
-        inRsA = new double[Max - Min];
-        onRsA = new double[Max - Min];
-        onRsFIELD = new double[Max - Min];
-        inRsFIELD = new double[Max - Min];
-        fieldDiff = new double[Max - Min];
-        RandFILEDset = new double[Max - Min, 2];
+        randoms = new double[wielkosc_tab];
+        inRsA = new double[wielkosc_tab];
+        onRsA = new double[wielkosc_tab];
+        onRsFIELD = new double[wielkosc_tab];
+        inRsFIELD = new double[wielkosc_tab];
+        fieldDiff = new double[wielkosc_tab];
+        RandFILEDset = new double[wielkosc_tab, 2];
 
+        iteracje = wielkosc_tab;
         MIN_NR = Min;
         MAX_NR = Max;
     }
@@ -145,9 +188,10 @@ class dataCenter
 
     public void arrayGenerator()
     {
-        for (int i = 0; i < MAX_NR - MIN_NR; i++)
+        for (int i = 0; i < iteracje; i++)
         {
             randoms[i] = rnd.Next(MIN_NR, MAX_NR);
+            //Console.WriteLine(randoms[i] + "\n");
         }
         //Console.WriteLine("GENERUJE! \n");
     }
@@ -159,7 +203,7 @@ class dataCenter
 
     public void onSqr()
     {
-        for (int i = 0; i < MAX_NR - MIN_NR; i++)
+        for (int i = 0; i < iteracje; i++)
         {
             onRsA[i] = 2.0 * randoms[i];
             onRsFIELD[i] = onRsA[i] * onRsA[i];
@@ -168,7 +212,7 @@ class dataCenter
 
     public void inSqr()
     {
-        for (int i = 0; i < MAX_NR - MIN_NR; i++)
+        for (int i = 0; i < iteracje; i++)
         {
             inRsA[i] = (2.0 * randoms[i]) / Math.Sqrt(2);
             inRsFIELD[i] = inRsA[i] * inRsA[i];
@@ -177,7 +221,7 @@ class dataCenter
 
     public void difference()
     {
-        for (int i = 0; i < MAX_NR - MIN_NR; i++)
+        for (int i = 0; i < iteracje; i++)
         {
             fieldDiff[i] = onRsFIELD[i] - inRsFIELD[i];
         }
@@ -185,16 +229,18 @@ class dataCenter
 
     public void calculateSet()
     {
-        for (int i = 0; i < MAX_NR - MIN_NR; i++)
+        for (int i = 0; i < iteracje; i++)
         {
             RandFILEDset[i, 0] = randoms[i];
             RandFILEDset[i, 1] = fieldDiff[i];
+            //Console.WriteLine(RandFILEDset[i, 0] + " " + RandFILEDset[i, 1] + "\n");
         }
     }
 
     Random rnd = new Random();
     public int MIN_NR = 0;
     public int MAX_NR = 0;
+    public int iteracje = 0;
     public double[] randoms;
     public double[] inRsA;
     public double[] inRsFIELD;
