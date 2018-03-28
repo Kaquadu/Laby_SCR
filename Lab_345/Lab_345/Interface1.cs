a64d1c0c264739d1b5b415da70f8fc72bcf8cf81
Toolmaster <kuba.kowalczykowski@student.put.poznan.pl>
2018-03-29 01:31:00 +0200

    Laby_345_Nieskonczone

---
+++ Lab_345/Lab_345/Interface1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_345
+{
+    interface IRunnable
+    {
+        void Run();
+        void CoroutineUpdate(float IEnumerator);
+        bool HasFinished { get; set; }
+    }
+
+    abstract class Agent : IRunnable
+    {
+
+        abstract public void Update();
+
+        abstract public void Run();
+        abstract public void CoroutineUpdate(float IEnumerator);
+        abstract public bool HasFinished { get; set; }
+
+    }
+
+    class ConstantCountingAgent : Agent
+    {
+        public ConstantCountingAgent(int Identify)
+        {
+            ID = Identify;
+            frequency = 10;
+        }
+        override public void Update()
+        {
+
+        }
+        override public void Run() { }
+        override public void CoroutineUpdate(float IEnumerator) { }
+        override public bool HasFinished { get; set; }
+
+        private int ID;
+        private int frequency;
+    }
+
+    class CountingAgent : Agent
+    {
+        public CountingAgent(int Identify)
+        {
+            ID = Identify;
+            frequency = 10;
+        }
+        override public void Update()
+        {
+            
+        }
+        override public void Run() { }
+        override public void CoroutineUpdate(float IEnumerator) { }
+        override public bool HasFinished { get; set; }
+
+        private int ID;
+        private int frequency;
+    }
+
+    class SineGeneratingAgent : Agent
+    {
+        public SineGeneratingAgent(int Identify)
+        {
+            ID = Identify;
+            frequency = 10;
+        }
+        override public void Update()
+        {
+
+        }
+        override public void Run() { }
+        override public void CoroutineUpdate(float IEnumerator) { }
+        override public bool HasFinished { get; set; }
+
+        private int ID;
+        private int frequency;
+        public double output;
+    }
+}
---
+++ Lab_345/Lab_345/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lab_345
+{
+    class Program
+    {
+        static int ilosc_agentow = 10;
+        static ConstantCountingAgent[] constantCountingAgent = new ConstantCountingAgent[ilosc_agentow];
+        static CountingAgent[] countingAgent = new CountingAgent[ilosc_agentow];
+        static SineGeneratingAgent[] sineGeneratingAgent = new SineGeneratingAgent[ilosc_agentow];
+
+        static void Main(string[] args)
+        {
+            GenerateRunnables();
+            RunThreads();
+        }
+
+        static void GenerateRunnables()
+        {     
+            for (int i = 0; i < ilosc_agentow; i++)
+            {
+                constantCountingAgent[i] = new ConstantCountingAgent(i);
+                countingAgent[i] = new CountingAgent(i + (ilosc_agentow));
+                sineGeneratingAgent[i] = new SineGeneratingAgent(i + (2 * (ilosc_agentow)));
+            }
+        }
+
+        static void RunThreads()
+        {
+            Thread[] threads = new Thread[ilosc_agentow * 3];
+
+            for (int i = 0; i < ilosc_agentow; i++)
+            {
+                threads[i] = new Thread(constantCountingAgent[i].Run);
+                threads[i+ilosc_agentow] = new Thread(countingAgent[i].Run);
+                threads[i+(2*ilosc_agentow)] = new Thread(sineGeneratingAgent[i].Run);
+            }
+
+        }
+    }
+}
